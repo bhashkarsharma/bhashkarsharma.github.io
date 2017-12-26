71ef70a05ba66c7d01e3511552cebc33a82af01b
Bhashkar Sharma <bhashkarsharma@gmail.com>
2017-12-26 14:04:41 +0100

    Code cleanup

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,24 +1,42 @@
-var cookieName = 'theme';
-var expires = new Date(Date.now() + 7 * 864e5).toUTCString();
+var SiteConf = (function() {
 
-function switchTheme(theme, persist=true) {
-    var switcher = document.querySelector('#themeswitcher');
-    var body = document.body;
-    var currTheme = body.classList.contains('day') ? 'day' : 'night';
-    var newTheme = theme ? theme : (currTheme === 'day' ? 'night' : 'day');
-    body.classList.remove(currTheme);
-    body.classList.add(newTheme);
-    if (persist) {
-        document.cookie = cookieName + '=' + encodeURIComponent(newTheme) + '; expires=' + expires + '; path=/';
+    var cookieName = 'theme';
+    var expires = new Date(Date.now() + 7 * 864e5).toUTCString();
+
+    function setCookie(key, val, expires) {
+        document.cookie = key + '=' + val + '; expires=' + expires + '; path=/';
     }
-}
+
+    function getCookie(key, defVal) {
+        return document.cookie.split('; ').reduce(function(k, v) {
+            var parts = v.split('=');
+            return parts[0] === key ? decodeURIComponent(parts[1]) : k;
+        }, defVal);
+    }
+
+    function switchTheme(theme, persist=true) {
+        var switcher = document.querySelector('#themeswitcher');
+        var body = document.body;
+        var currTheme = body.classList.contains('day') ? 'day' : 'night';
+        var newTheme = theme ? theme : (currTheme === 'day' ? 'night' : 'day');
+        body.classList.remove(currTheme);
+        body.classList.add(newTheme);
+        if (persist) {
+            setCookie(cookieName, encodeURIComponent(newTheme), expires);
+        }
+    }
+
+    return {
+        cookieName,
+        setCookie,
+        getCookie,
+        switchTheme
+    };
+})();
 
 (function() {
-    var theme = document.cookie.split('; ').reduce(function(k, v) {
-        var parts = v.split('=');
-        return parts[0] === cookieName ? decodeURIComponent(parts[1]) : k;
-    }, 'night');
-    if (theme.length > 0) switchTheme(theme);
+    var theme = SiteConf.getCookie(SiteConf.cookieName, 'night');
+    if (theme.length > 0) SiteConf.switchTheme(theme);
 })();
 
 (function() {
